068139b04d4cb03fbf7724574ec6756f77117e96
Iván Córdova <iacordovamorales@gmail.com>
2014-09-15 01:10:26 -0500

    Footer layout tweaked

--- views/footer.php
+++ views/footer.php
@@ -6,68 +6,72 @@
 	<!-- / wrapper -->
 
 	<!-- footer -->
-	<div id="footer" class="clearingfix">
+	<div id="footer" class="row">
 
-		<div id="underfooter"></div>
+		<div id="underfooter" class="large-12 columns"></div>
 
 		<!-- footer content -->
-		<div class="wrapper floatholder rapidxwpr">
+		<div class="large-12 columns">
 
-			<!-- footer credits -->
-			<div class="footer-credits">
-				Powered by the &nbsp;
-				<a href="http://www.ushahidi.com/">
-					<img src="<?php echo url::file_loc('img'); ?>media/img/footer-logo.png" alt="Ushahidi" class="footer-logo" />
-				</a>
-				&nbsp; Platform
-			</div>
-			<!-- / footer credits -->
+			<div class="row">
 
-			<!-- footer menu -->
-			<div class="footermenu">
-				<ul class="clearingfix">
-					<li>
-						<a class="item1" href="<?php echo url::site(); ?>">
-							<?php echo Kohana::lang('ui_main.home'); ?>
-						</a>
-					</li>
-
-					<?php if (Kohana::config('settings.allow_reports')): ?>
-					<li>
-						<a href="<?php echo url::site()."reports/submit"; ?>">
-							<?php echo Kohana::lang('ui_main.submit'); ?>
-						</a>
-					</li>
-					<?php endif; ?>
-					
-					<?php if (Kohana::config('settings.allow_alerts')): ?>
+				<!-- footer menu -->
+				<div class="footermenu large-8 columns">
+					<ul>
 						<li>
-							<a href="<?php echo url::site()."alerts"; ?>">
-								<?php echo Kohana::lang('ui_main.alerts'); ?></a>
+							<a class="item1" href="<?php echo url::site(); ?>">
+								<?php echo Kohana::lang('ui_main.home'); ?>
+							</a>
 						</li>
-					<?php endif; ?>
 
-					<?php if (Kohana::config('settings.site_contact_page')): ?>
-					<li>
-						<a href="<?php echo url::site()."contact"; ?>">
-							<?php echo Kohana::lang('ui_main.contact'); ?>
-						</a>
-					</li>
-					<?php endif; ?>
+						<?php if (Kohana::config('settings.allow_reports')): ?>
+						<li>
+							<a href="<?php echo url::site()."reports/submit"; ?>">
+								<?php echo Kohana::lang('ui_main.submit'); ?>
+							</a>
+						</li>
+						<?php endif; ?>
+						
+						<?php if (Kohana::config('settings.allow_alerts')): ?>
+							<li>
+								<a href="<?php echo url::site()."alerts"; ?>">
+									<?php echo Kohana::lang('ui_main.alerts'); ?></a>
+							</li>
+						<?php endif; ?>
 
-					<?php
-					// Action::nav_main_bottom - Add items to the bottom links
-					Event::run('ushahidi_action.nav_main_bottom');
-					?>
-				</ul>
+						<?php if (Kohana::config('settings.site_contact_page')): ?>
+						<li>
+							<a href="<?php echo url::site()."contact"; ?>">
+								<?php echo Kohana::lang('ui_main.contact'); ?>
+							</a>
+						</li>
+						<?php endif; ?>
 
-				<?php if ($site_copyright_statement != ''): ?>
-	      		<p><?php echo $site_copyright_statement; ?></p>
-		      	<?php endif; ?>
-		      	
+						<?php
+						// Action::nav_main_bottom - Add items to the bottom links
+						Event::run('ushahidi_action.nav_main_bottom');
+						?>
+					</ul>
+
+					<?php if ($site_copyright_statement != ''): ?>
+		      		<p><?php echo $site_copyright_statement; ?></p>
+			      	<?php endif; ?>
+			      	
+				</div>
+				<!-- / footer menu -->
+
+				<!-- footer credits -->
+				<div class="large-4 columns">
+					Powered by the &nbsp;
+					<a href="http://www.ushahidi.com/">
+						<img src="<?php echo url::file_loc('img'); ?>media/img/footer-logo.png" alt="Ushahidi" class="footer-logo" />
+					</a>
+					&nbsp; Platform
+				</div>
+				<!-- / footer credits -->
+
 			</div>
-			<!-- / footer menu -->
-
+			<!--  / row -->
 
 		</div>
 		<!-- / footer content -->
